8ee47339eb462acf481f31b2eba1cd967be84759
sebasruiz09 <sebasruiz909@gmail.com>
2022-07-01 09:41:12 -0500

    estructura de proyecto formativo Easy House Rent

---
+++ Codigo Fuente/FRONTEND_EHR/src/Components/ApiRoutes.js
@@ -0,0 +1,6 @@
+export const urlUsers = "https://easy-house-rent.azurewebsites.net/api/Users";
+export const urLDepartments = "https://easy-house-rent.azurewebsites.net/api/Department";
+export const urlMunicipality = "https://easy-house-rent.azurewebsites.net/api/Municipality/0?iddepartamento=";
+export const urlLogin = "https://easy-house-rent.azurewebsites.net/api/Login"
+export const urlSearch = "https://easy-house-rent.azurewebsites.net/api/home"
+export const urlAdversitement = 'https://easy-house-rent.azurewebsites.net/api/Advertisement/'
