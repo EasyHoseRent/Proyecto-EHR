5d957e6931e9af53cdbe2840fc50493b61225f10
sebasruiz09 <sebasruiz909@gmail.com>
2022-10-18 19:40:47 -0500

    UPDATE : changes in footer links redirect

--- Codigo Fuente/FRONTEND_EHR/src/Components/Layout/Footer/Footer.jsx
+++ Codigo Fuente/FRONTEND_EHR/src/Components/Layout/Footer/Footer.jsx
@@ -13,8 +13,8 @@ export const Footer = () => {
         <div className="footer-icons">
           <a href='https://twitter.com/EasyHouseRent02' target='_blank' rel='noreferrer' ><FontAwesomeIcon className="footer-icon" icon={faTwitter}/></a>
           <a href='https://www.facebook.com/Easy-House-Rent-104807078925662' target='_blank' rel='noreferrer' ><FontAwesomeIcon className="footer-icon" icon={faFacebook}/></a>
-          <a href='https://wa.link/l1zwok' target='_blank' rel='noreferrer'><FontAwesomeIcon className="footer-icon" icon={faWhatsapp}/></a>
-          <a href='https://github.com/sebasruiz09' target='_blank' rel='noreferrer'><FontAwesomeIcon  className='footer-icon' icon={faGithub}/></a>
+          <a href='https://api.whatsapp.com/send?phone=573127104558&text=%F0%9F%92%81%E2%80%8D%E2%99%80%EF%B8%8F%20Este%20es%20un%20mensaje%20predeterminado%20al%20servicio%20al%20cliente%20de%20EHR%20%2C%20%20dejanos%20tu%20consulta.%0Aen%20breve%20te%20respondemos' target='_blank' rel='noreferrer'><FontAwesomeIcon className="footer-icon" icon={faWhatsapp}/></a>
+          <a href='https://github.com/mari293' target='_blank' rel='noreferrer'><FontAwesomeIcon  className='footer-icon' icon={faGithub}/></a>
           <a href='https://www.instagram.com/easyhouserent' target='_blank' rel='noreferrer'><FontAwesomeIcon className='footer-icon' icon={faInstagram}/></a>     
         </div>
         <div className="footer-relinks">
